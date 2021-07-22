a30cab6a518c67fcc7946bd1a543a15b0bee4a20
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2021-07-22 11:04:14 -0400

    Updated epoch time to be correct

--- src/assets/gameShow/gameInfo.js
+++ src/assets/gameShow/gameInfo.js
@@ -1,6 +1,6 @@
 const info = {
   subject: 'Paper Wallet History',
-  startEpochTime: '1627430400',
+  startEpochTime: '1627603200',
   nextShow: '1713785405',
   donatationAmountUSD: '100',
   crypto: 'Bitcoin (BTC)',
